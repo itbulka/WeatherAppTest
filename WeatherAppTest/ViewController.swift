20da6b4d80b879e6c1c31797574da1cce045839d
itBulka <lokireads@gmail.com>
2022-10-07 00:41:16 +0300

    Initial Commit

---
+++ WeatherAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppTest
+//
+//  Created by Владимир Повальский on 07.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
