5b1fd3548df1e55903a1fb8ef98254d9e0cf6245
itBulka <lokireads@gmail.com>
2022-10-08 22:33:29 +0300

    Design FavoriteController

---
+++ WeatherAppTest/Controller/Favorite/Cell/FavoriteCell.swift
@@ -0,0 +1,97 @@
+//
+//  FavoriteCell.swift
+//  WeatherAppTest
+//
+//  Created by Владимир Повальский on 08.10.2022.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+    
+    static let identifier: String = "FavoriteCell"
+    
+    private let viewCell: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 12
+        view.layer.shadowColor = Resources.Colors.lightGray.cgColor
+        view.layer.shadowRadius = 6
+        view.layer.shadowOffset = .zero
+        view.layer.shadowOpacity = 1.0
+        return view
+    }()
+    
+    private let nameCity: UILabel = {
+        let label = UILabel()
+        label.text = "Moscow"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 14, weight: .semibold, width: .standard)
+        return label
+    }()
+    
+    private let temperature: UILabel = {
+        let label = UILabel()
+        label.textColor = Resources.Colors.hotWeather
+        label.text = "22"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .semibold, width: .standard)
+        return label
+    }()
+    
+    private let weatherCondition: UILabel = {
+        let label = UILabel()
+        label.textColor = Resources.Colors.lightGray
+        label.text = "Облачно, ветер: 2 м/с"
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold, width: .standard)
+        return label
+    }()
+    
+    private let buttonLike: UIButton = {
+        let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        btn.setImage(Resources.Icons.like, for: .normal)
+        btn.tintColor = .systemRed
+        return btn
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Error")
+    }
+    
+    private func configureCell() {
+        addView(viewCell)
+        viewCell.addView(temperature)
+        viewCell.addView(nameCity)
+        viewCell.addView(weatherCondition)
+        viewCell.addView(buttonLike)
+        
+        constraintLayout()
+    }
+    
+    private func constraintLayout() {
+        NSLayoutConstraint.activate([
+            viewCell.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            viewCell.leftAnchor.constraint(equalTo: leftAnchor, constant: 20),
+            viewCell.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            viewCell.rightAnchor.constraint(equalTo: rightAnchor, constant: -20),
+            
+            temperature.leftAnchor.constraint(equalTo: viewCell.leftAnchor, constant: 16),
+            temperature.topAnchor.constraint(equalTo: viewCell.topAnchor, constant: 16),
+            
+            nameCity.topAnchor.constraint(equalTo: viewCell.topAnchor, constant: 16),
+            nameCity.leftAnchor.constraint(equalTo: temperature.rightAnchor, constant: 10),
+            
+            weatherCondition.topAnchor.constraint(equalTo: nameCity.bottomAnchor, constant: 2),
+            weatherCondition.leftAnchor.constraint(equalTo: nameCity.leftAnchor),
+            
+            buttonLike.rightAnchor.constraint(equalTo: viewCell.rightAnchor, constant: -16),
+            buttonLike.centerYAnchor.constraint(equalTo: viewCell.centerYAnchor)
+            
+        ])
+    }
+    
+}
--- WeatherAppTest/Controller/Favorite/FavoriteViewController.swift
+++ WeatherAppTest/Controller/Favorite/FavoriteViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class FavoriteViewController: BaseViewController {
+    
+    private let tableFavoriteCities: UITableView = {
+        let table = UITableView()
+        table.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.identifier)
+        table.separatorStyle = .none
+        table.allowsSelection = false
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = Resources.Titles.favoriteTitle
         
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableFavoriteCities.frame = view.bounds
+    }
 
 
+}
+
+extension FavoriteViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.identifier, for: indexPath) as? FavoriteCell else { return UITableViewCell()}
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    override func configureUI() {
+        super.configureUI()
+        tableFavoriteCities.delegate = self
+        tableFavoriteCities.dataSource = self
+    }
+    
+    override func addViews() {
+        super.addViews()
+        view.addView(tableFavoriteCities)
+    }
+    
+    override func layoutConstraints() {
+        super.layoutConstraints()
+    }
 }
